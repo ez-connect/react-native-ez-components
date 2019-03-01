c8c108e0ea693369c0476625905970c315c329db
Thanh Vinh <thanh.vinh@hotmail.com>
2019-03-01 22:34:08 +0700

    NavigationService: allows screen tracking

--- dist/NavigationService.js
+++ dist/NavigationService.js
@@ -1,16 +1,16 @@
 import { NavigationActions, StackActions } from 'react-navigation';
 export class NavigationService {
     static setTopLevelNavigator(navigatorRef) {
-        NavigationService.s_navigator = navigatorRef;
+        NavigationService._navigator = navigatorRef;
     }
     static navigate(routeName, params) {
-        NavigationService.s_navigator.dispatch(NavigationActions.navigate({
+        NavigationService._navigator.dispatch(NavigationActions.navigate({
             routeName,
             params,
         }));
     }
     static push(routeName, params) {
-        NavigationService.s_navigator.dispatch(StackActions.push({
+        NavigationService._navigator.dispatch(StackActions.push({
             routeName,
             params,
         }));
@@ -20,9 +20,30 @@ export class NavigationService {
             index: 0,
             actions: [NavigationActions.navigate({ routeName, params })],
         });
-        NavigationService.s_navigator.dispatch(resetAction);
+        NavigationService._navigator.dispatch(resetAction);
     }
     static goBack() {
-        NavigationService.s_navigator.dispatch(NavigationActions.back());
+        console.warn(NavigationActions.back());
+        NavigationService._navigator.dispatch(NavigationActions.back());
+    }
+    static getPrevRoute() {
+        return this._prevRoute;
+    }
+    static getCurrentRoute() {
+        return this._currentRoute;
+    }
+    static handleOnNavigationStateChange(prevState, currentState, action) {
+        NavigationService._prevRoute = NavigationService._getActiveRouteName(prevState);
+        NavigationService._currentRoute = NavigationService._getActiveRouteName(currentState);
+    }
+    static _getActiveRouteName(navigationState) {
+        if (!navigationState) {
+            return null;
+        }
+        const route = navigationState.routes[navigationState.index];
+        if (route.routes) {
+            return NavigationService._getActiveRouteName(route);
+        }
+        return route.routeName;
     }
 }
--- src/NavigationService.ts
+++ src/NavigationService.ts
@@ -1,12 +1,12 @@
-import { NavigationActions, NavigationContainerComponent, StackActions } from 'react-navigation';
+import { NavigationAction, NavigationActions, NavigationContainerComponent, NavigationState, StackActions } from 'react-navigation';
 
 export class NavigationService {
   public static setTopLevelNavigator(navigatorRef: NavigationContainerComponent) {
-    NavigationService.s_navigator = navigatorRef;
+    NavigationService._navigator = navigatorRef;
   }
 
   public static navigate(routeName: string, params?: any) {
-    NavigationService.s_navigator.dispatch(
+    NavigationService._navigator.dispatch(
       NavigationActions.navigate({
         routeName,
         params,
@@ -15,7 +15,7 @@ export class NavigationService {
   }
 
   public static push(routeName: string, params?: any) {
-    NavigationService.s_navigator.dispatch(
+    NavigationService._navigator.dispatch(
       StackActions.push({
         routeName,
         params,
@@ -28,13 +28,44 @@ export class NavigationService {
       index: 0,
       actions: [NavigationActions.navigate({ routeName, params })],
     });
-    NavigationService.s_navigator.dispatch(resetAction);
+    NavigationService._navigator.dispatch(resetAction);
   }
 
   public static goBack() {
-    NavigationService.s_navigator.dispatch(NavigationActions.back());
+    console.warn(NavigationActions.back());
+    NavigationService._navigator.dispatch(NavigationActions.back());
   }
 
-  // tslint:disable-next-line:variable-name
-  private static s_navigator: NavigationContainerComponent;
+  public static getPrevRoute(): string {
+    return this._prevRoute;
+  }
+
+  public static getCurrentRoute(): string {
+    return this._currentRoute;
+  }
+
+  public static handleOnNavigationStateChange(prevState: NavigationState, currentState: NavigationState, action: NavigationAction) {
+    NavigationService._prevRoute = NavigationService._getActiveRouteName(prevState);
+    NavigationService._currentRoute = NavigationService._getActiveRouteName(currentState);
+  }
+
+  ///////////////////////////////////////////////////////////////////
+
+  private static _navigator: NavigationContainerComponent;
+  private static _prevRoute?: string;
+  private static _currentRoute?: string;
+
+  // Gets the current screen from navigation state
+  // https://reactnavigation.org/docs/en/screen-tracking.html#listening-to-state-changes
+  private static _getActiveRouteName(navigationState /*: NavigationState */) {
+    if (!navigationState) {
+      return null;
+    }
+    const route = navigationState.routes[navigationState.index];
+    // dive into nested navigators
+    if (route.routes) {
+      return NavigationService._getActiveRouteName(route);
+    }
+    return route.routeName;
+  }
 }
