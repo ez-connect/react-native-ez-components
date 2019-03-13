67fe2f83cb02d18799def81ea5a887844d4cb2a8
Thanh Vinh <thanh.vinh@hotmail.com>
2019-03-13 19:11:44 +0700

    Upgrade to RN v0.59.0, use @react-native-community/async-storage

--- dist/Storage.js
+++ dist/Storage.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 export class Storage {
     static async save(key, item, excludes = [], encodeHandler) {
         Storage._logEnabled && console.info(`Storage.save: ${key}`);
